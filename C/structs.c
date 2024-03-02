706c485e7681e6b38113ea05686eb00ff3aaa9ea
ChrisH62 <88059992+ChrisH62@users.noreply.github.com>
2024-03-02 13:36:33 -0500

    test2

--- C/structs.c
+++ C/structs.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-typedef struct
-{
-    unsigned int age;
-    char name[50];
+typedef struct {
+  unsigned int age;
+  char name[50];
 } myStruct;
 
-int main(void)
-{
-    // Need the struct keyword to create a member
-     myStruct s1;
+int main(void) {
+  // Need the struct keyword to create a member
+  myStruct s1;
 
-    s1.age = 22;
-// Originally: s1.name[] = "Chris"
-// Learned that you cannot assign strings, and you have to "copy" into them
-    strlcpy(s1.name, "Chris", 6);
+  s1.age = 22;
+  // Originally: s1.name[] = "Chris"
+  // Learned that you cannot assign strings, and you have to "copy" into them
+  strlcpy(s1.name, "Chris", 6);
+
+  printf("%s - Age %d\n", s1.name, s1.age);
 }
